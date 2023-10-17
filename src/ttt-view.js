cbf5cab57d1293c93c95edcd1bc2d3f69e4ac8b1
Balaji <imnotbalaji@gmail.com>
2023-10-17 15:27:26 -0400

    complete phase 1

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -4,29 +4,48 @@ class View {
     this.game = game;
 
     this.setupBoard();
+    // this.handleGameOver();
   }
   
   setupBoard() {
     let grid = document.createElement("ul");
     this.figure.appendChild(grid);
 
-    let cell = [];
+    // let cell = [];
     
-    for(let i = 0; i < 9; i++){
-      cell.push(document.createElement("li"));
-      grid.appendChild(cell[i]);
+    for(let i = 0; i < 3; i++){
+      for (let j = 0 ; j < 3; j++){
+        let cell = document.createElement("li");
+        cell.dataset.row = i;
+        // cell.setAttribute("row",i);
+        cell.dataset.column =j;
+        grid.appendChild(cell);
+      }
     }
+    grid.addEventListener("click",this.handleClick.bind(this));
+  
   }
   
   handleClick(e) {
-    // let pos = 
-    this.game.__proto__.playMove(pos)
+    let click_row = e.target.dataset.row;
+    let click_column = e.target.dataset.column;
+    this.game.playMove([click_row,click_column]);
+    this.makeMove(e.target);
+    this.handleGameOver();
+    
   }
 
   makeMove(square) {
+  
+    square.classList.add("played-position");  
+     square.innerText = `${this.game.currentPlayer}`;
   }
   
+  
   handleGameOver() {
+    if (this.game.isOver()) {
+    alert(this.game.winner()) ;
+    }
   }
 }
 
